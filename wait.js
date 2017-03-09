0d6c355fc6eb1e0252b67aebd29516d525a40fd1
Matthew Phillips <matthew@matthewphillips.info>
2017-03-08 19:26:00 -0500

    preventDefault and stopPropagation works

--- wait.js
+++ wait.js
@@ -1,36 +1,47 @@
-let ORIGINAL_TARGET = Symbol('event-wait-until-original-target');
+(function(){
+  let ORIGINAL_TARGET = Symbol('event-wait-until-original-target');
 
-self.eventWaitUntil = function(event) {
-  let originalTarget = event.originalTarget || event.target;
-  // We need to dispatch on the parent, probably
-  let ct = event.currentTarget.parentNode;
-  let path = [].slice.call(event.path);
-  event.stopPropagation();
-  event.preventDefault();
+  self.eventWaitUntil = function(event) {
+    let originalTarget = event.originalTarget || event.target;
+    // We need to dispatch on the parent, probably
+    let ct = event.currentTarget.parentNode;
+    let path = [].slice.call(event.path);
+    event.stopPropagation();
+    event.preventDefault();
 
-  return function(promise){
-    Promise.resolve(promise)
-    .then(function(){
-      let FancyEvent = class extends Event {
-        get path() {
-          return path;
-        }
-        get target() {
-          return originalTarget;
-        }
-        get [ORIGINAL_TARGET]() {
-          return originalTarget
-        }
-      };
+    let propagationStopped = false;
+    event.stopPropagation = () => propagationStopped = true;
+    let defaultPrevented = false;
+    event.preventDefault = () => defaultPrevented = true;
 
-      let newEvent = new FancyEvent(event.type, {
-        bubbles: event.bubbles,
-        cancelable: event.cancelable,
-        scoped: event.scoped,
-        composed: event.composed
+    return function(promise){
+      return Promise.resolve(promise)
+      .then(function(){
+        if(propagationStopped || defaultPrevented) {
+          return;
+        }
+
+        let FancyEvent = class extends Event {
+          get path() {
+            return path;
+          }
+          get target() {
+            return originalTarget;
+          }
+          get [ORIGINAL_TARGET]() {
+            return originalTarget
+          }
+        };
+
+        let newEvent = new FancyEvent(event.type, {
+          bubbles: event.bubbles,
+          cancelable: event.cancelable,
+          scoped: event.scoped,
+          composed: event.composed
+        });
+
+        ct.dispatchEvent(newEvent);
       });
-
-      ct.dispatchEvent(newEvent);
-    });
+    };
   };
-};
+})();
